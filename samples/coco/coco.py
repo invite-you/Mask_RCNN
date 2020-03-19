94f47a00a186214c8f12a9d5d4362e952970a002
invite-you <50567167+invite-you@users.noreply.github.com>
2020-03-20 02:20:09 +0900

    Update coco.py

--- samples/coco/coco.py
+++ samples/coco/coco.py
@@ -431,7 +431,7 @@ if __name__ == '__main__':
                         metavar="/path/to/logs/",
                         help='Logs and checkpoints directory (default=logs/)')
     parser.add_argument('--limit', required=False,
-                        default=500,
+                        default=99999,
                         metavar="<image count>",
                         help='Images to use for evaluation (default=500)')
     parser.add_argument('--download', required=False,
